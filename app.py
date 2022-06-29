d93b4f695eb16fb7db41d965a3acf4153bffca04
hzha559 <819601045@qq.com>
2022-06-29 15:34:48 +1000

    complete front end

--- app.py
+++ app.py
@@ -43,36 +43,48 @@ class UserService:
         for user in cls.users:
             if user_id == user['id']:
                 return user
+def updatedel(statement,filter):
+    conn = pymssql.connect(host='127.0.0.1', user='sa', password='12345678', database='1')
+    cur = conn.cursor()
+    query = statement+filter
+    try:
+        #logging.warning(query)
+        cur.execute(query)
+        conn.commit()
+        return 0
+    except Exception as e:
+        return e
 
-def queryall():
+
+
+def queryall(filter):
     conn = pymssql.connect(host='127.0.0.1', user='sa', password='12345678', database='1')
     cur = conn.cursor()
     query = 'SELECT * FROM [1].[dbo].[appointment] '
-    # query += 'where month=\'2022-02\' and assetid like \'%-EM-%\''
-    # query = query.replace('-EM-', meter)
-    # query = query.replace('2022-02', month)
+    query+=filter
     try:
-        cur.execute(query)
-        result = cur.fetchall()
-        result1 = []
-        trendkey = []
-        for i in range(len(result)):
-            result1.append([])
-            trendkey.append([])
-            trendkey[i].append(result[i][0])
-            result1[i].append(result[i][0])
-            result1[i].append(result[i][1])
-            result1[i].append(result[i][2])
-            result1[i].append(result[i][3])
-            result1[i].append(str(result[i][4]))
-            result1[i].append(str(result[i][5]))
-            result1[i].append(str(result[i][6]))
-        # logging.warning(result1)
-        result2 = {'key': trendkey, 'data': result1}
-        user = {'vav': result2}
+            #logging.warning(query)
+            cur.execute(query)
+            result = cur.fetchall()
+            result1 = []
+            trendkey = []
+            for i in range(len(result)):
+                result1.append([])
+                trendkey.append([])
+                trendkey[i].append(result[i][0])
+                result1[i].append(result[i][0])
+                result1[i].append(result[i][1])
+                result1[i].append(result[i][2])
+                result1[i].append(result[i][3])
+                result1[i].append(str(result[i][4]))
+                result1[i].append(str(result[i][5]))
+                result1[i].append(str(result[i][6]))
+            # logging.warning(result1)
+            result2 = {'key': trendkey, 'data': result1}
+            user = {'vav': result2,'submit':0}
     except Exception as e:
-        result2 = {'key': e, 'data': e}
-        user={'vav': result2}
+            result2 = {'key': e, 'data': e}
+            user={'vav': result2,'submit':0}
     return user
 
 # 1、实例化登录管理对象
@@ -91,44 +103,78 @@ class User(UserMixin):
     pass
 
 @app.route('/delta', methods=['GET', 'POST'])
+@login_required
 def change():
     if request.method == 'POST':
         if request.json['id'][0:4]=='dele':
-            conn = pymssql.connect(host='127.0.0.1', user='sa', password='12345678', database='1')
-            cur = conn.cursor()
             query = 'delete from Appointment '
-            query += 'where trendkey=125'
-            query = query.replace('125', request.json['id'][4:])
-            try:
-                cur.execute(query)
-            except Exception as e:
-                logging.warning(e)
-            conn.commit()
-            return jsonify(queryall())
+            query1 = 'where trendkey=125'
+            query1 = query1.replace('125', request.json['id'][4:])
+            updatedel(query,query1)
+            result = queryall('')
+            Thread(target=post_to_carpark, args=(result,)).start()
+            return jsonify(result)
         elif request.json['id'][0:4]=='save':
-            conn = pymssql.connect(host='127.0.0.1', user='sa', password='12345678', database='1')
-            cur = conn.cursor()
             rego=request.json['rego']
             data=request.json['data']
-
             query='update Appointment set[rego] = \'1pv3oh\''
-            query+='where email = \'123@hotmail.com\' and convert(nvarchar(MAX), datetime, 120) = \'2022-03-01\''
+            query1='where email = \'123@hotmail.com\' and convert(nvarchar(MAX), datetime, 120) = \'2022-03-01\''
             query=query.replace('1pv3oh',rego)
-            query = query.replace('123@hotmail.com',data[3])
-            query = query.replace('2022-03-01', data[5])
-            try:
-                cur.execute(query)
-            except Exception as e:
-                logging.warning(e)
-            conn.commit()
-            return jsonify(queryall())
+            query1 = query1.replace('123@hotmail.com',data[3])
+            query1 = query1.replace('2022-03-01', data[5])
+
+            updatedel(query,query1)
+            result=queryall('')
+            #logging.warning(result['submit'])
+            #Thread(target=post_to_carpark, args=(result,)).start()
+            return jsonify(result)
+
+        else:
+            return jsonify(queryall(''))
     return 1
 
+@app.route('/deltaclient', methods=['GET', 'POST'])
+def change1():
+    if request.method == 'POST':
+        logging.warning(request.json['id'])
+        if request.json['id'][0:4]=='sear':
+            email=request.json['email']
+            date=request.json['date']
+            #logging.warning(email,date)
+            #query='select * from Appointment '
+            query=' where email = \'123@hotmail.com\' and left(convert(nvarchar(MAX), datetime, 120),10) = \'2022-03-01\''
+
+            query = query.replace('123@hotmail.com',email)
+            query = query.replace('2022-03-01', date)
+            #updatedel(query)
+            return jsonify(queryall(query))
+        elif request.json['id']=='update':
+            email = request.json['email']
+            date = request.json['date']
+            rego=request.json['rego']
+            # logging.warning(email,date)
+            # query='select * from Appointment '
+            query = 'update Appointment set[rego] = \'1pv3oh\''
+            query1= ' where email = \'123@hotmail.com\' and left(convert(nvarchar(MAX), datetime, 120),10) = \'2022-03-01\''
+            query = query.replace('1pv3oh', rego)
+            query1 = query1.replace('123@hotmail.com', email)
+            query1 = query1.replace('2022-03-01', date)
+            #logging.warning(query)
+            updatedel(query,query1)
+            return jsonify(queryall(query1))
+        else:
+            return jsonify(' ')
+            # logging.warning(queryall(query))
+
+    return '1'
+
+@app.route('/delta1', methods=['GET', 'POST'])
 def post_to_carpark():
-    from math import factorial
-    time.sleep(100)
-    print('posted to carpark')
-    return 0
+    result = queryall('')
+    result['submit']=1
+    time.sleep(10)
+    print('posted to carpark ',result['submit'])
+    return jsonify(result)
 
 @app.route('/pms', methods=['GET', 'POST'])
 def receive_post():
@@ -148,7 +194,7 @@ def receive_post():
         except Exception as e:
             logging.warning(e)
         conn.commit()
-        Thread(target = post_to_carpark).start()
+        #Thread(target = post_to_carpark).start()
         return '200'
     else:
         return '401'
@@ -202,6 +248,7 @@ def logout():
     logout_user()
     return 'Logged out successfully!'
 
+#for patient
 @app.route('/')
 def index1():
     conn = pymssql.connect(host='127.0.0.1', user='sa', password='12345678', database='1')
@@ -224,8 +271,8 @@ def index1():
         result1[i].append(str(result[i][4]))
         result1[i].append(str(result[i][5]))
     #logging.warning(result1)
-    result2={'key':trendkey,'data':result1}
-    user= {'vav':result2}
+    result2={'key':'','data':''}
+    user= {'vav':result2,'submitted':0}
     response = Response(render_template("visitor.html",
                                         title='Home',
                                         user=user))
@@ -259,7 +306,7 @@ def index():
         result1[i].append(str(result[i][6]))
     # logging.warning(result1)
     result2 = {'key': trendkey, 'data': result1}
-    user = {'vav': result2}
+    user = {'vav': result2,'submitted':0}
     response = Response(render_template("blank.html",
                                         title='Home',
                                         user=user))
@@ -273,12 +320,7 @@ def message():
 def notfound():
     return render_template('404.html')
 
-@app.route('/delta',methods=['POST','GET'])
-def delta():
-    result1=[[3, 'leo', 401001001, '1st3tt', '2022-04-01 07:00:00', '2022-03-01 00:00:00']]
-    user = {'vav': result1}
 
-    return jsonify(user)
 
 if __name__ == '__main__':
     #serve(app, host='0.0.0.0', port=2556)
